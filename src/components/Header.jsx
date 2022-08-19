f79ff7e9f3674f9172ce7e5a39529ac9455b478d
godzilli <jenniferdacostazilli@hotmail.com>
2022-08-19 00:44:19 -0300

    Inserido logo no header e estilizacao no css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import styles from './Header.module.css'
+import logoHeader from '../../imagens/vite.svg';
 
 export function Header(){
     return(
         <header className={styles.header}>
-            <strong>Feed</strong>
+            <img src={logoHeader} alt="Logotipo da minha aplicacao" />
         </header>
     );
 }
 //componentes sempre com Letra maíuscula para não misturar com tags do html
 //nao usa 'class' dentro dos elementos, utiliza className
 //{} quando eu quero colocar uma variavel Javascript dentro do HTML
+//1rem é 16px no padrão html
